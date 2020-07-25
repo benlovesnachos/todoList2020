39565abb05a3fda6d7141a18bc6e7d979d4518a5
Benjamin Rodriguez <ben@itnachos.com>
2020-07-25 12:37:31 -0500

    Completed step 6

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -3,8 +3,12 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-input-button-unit',
   // templateUrl: './input-button-unit.component.html',
-  template: `<p>input-button-unit works!</p>
-    The title is: {{ title }} `,
+  template: `
+    <p>input-button-unit works!</p>
+    The title is: {{ title }}
+    <input [value]="title" />
+    <button>Save</button>
+  `,
   styleUrls: ['./input-button-unit.component.css'],
 })
 export class InputButtonUnitComponent implements OnInit {
@@ -16,6 +20,9 @@ export class InputButtonUnitComponent implements OnInit {
 
   ngOnInit(): void {
     this.changeTitle('Angular CLI Rules.');
+    setTimeout(() => {
+      this.changeTitle('This is not the title you are looking for');
+    }, 3000);
   }
 
   changeTitle(newTitle: string) {
